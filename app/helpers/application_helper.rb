ca9b0013cd442f82e592376445eb828d0ff964ff
dfxdxx <1817556010@qq.com>
2016-11-10 06:28:20 -0800

    xxxx

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+		base_title = 'Just for fun'
+		if page_title.empty?
+			base_title
+		else
+			"%s | %s" % [page_title, base_title]
+		end
+	end
 end
